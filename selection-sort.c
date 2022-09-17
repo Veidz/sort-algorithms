096c508942ca89c51efb0999f32c798940f8d08f
Veidz <joaovictorveidz@gmail.com>
2022-09-17 18:19:57 -0300

    feat: add dynamic array size implementation

--- selection-sort.c
+++ selection-sort.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
+void initializeArray(int *array, int length);
 void insertionSort(int array[], int length);
 void printArray(int array[], int length);
 
 int main() {
-  int array[] = { 5, 3, 1, 8, 0 };
-  int length = sizeof(array) / sizeof(array[0]);
+  int *array;
+  int length = 10;
 
-  printf("Original Array\n");
-  printArray(array, length);
+  array = (int*)malloc(sizeof(int) * length);
 
-  clock_t start = clock();
-  insertionSort(array, length);
-  clock_t end = clock();
+  if (array != NULL) {
+    initializeArray(array, length);
 
-  printf("\n\nSorted Array\n");
-  printArray(array, length);
+    printf("Original Array\n");
+    printArray(array, length);
 
-  printf("\n\nElapsed Time: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+    clock_t start = clock();
+    insertionSort(array, length);
+    clock_t end = clock();
+
+    printf("\n\nSorted Array\n");
+    printArray(array, length);
+
+    printf("\n\nElapsed Time: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+  }
+  else {
+    printf("Memory not allocated\n");
+    exit(0);
+  }
+}
+
+void initializeArray(int *array, int length) {
+  srand(time(NULL));
+  for (int i = 0; i < length; i++) {
+    array[i] = rand() % 10;
+  }
 }
 
 void printArray(int array[], int length) {
