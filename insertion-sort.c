cf90bb9dce058af98d5cc6bb5749b3d89db68e9d
Veidz <joaovictorveidz@gmail.com>
2022-09-17 15:45:13 -0300

    refactor: create a function to isolate the sort algorithm

--- insertion-sort.c
+++ insertion-sort.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
+void insertionSort(int array[], int length);
+
 int main() {
   int array[] = { 5, 3, 1, 8, 0 };
   int length = sizeof(array) / sizeof(array[0]);
 
+  insertionSort(array, length);
+
+  for (int i = 0; i < length; i++) {
+    printf("%d ", array[i]);
+  }
+}
+
+void insertionSort(int array[], int length) {
   int i, j, aux;
 
   for (i = 1; i < length; i++) {
     aux = array[i];
 
-    for (j = i; aux < array[j - 1]; j--) {
+    for (j = i; (j > 0) && (aux < array[j - 1]); j--) {
       array[j] = array[j - 1];
     }
 
     array[j] = aux;
-  }
-
-  for (i = 0; i < length; i++) {
-    printf("%d ", array[i]);
   }
 }
