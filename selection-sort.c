9f79ad56e5ef3bee307b2bbf5aa04c3cc6bcd4cd
Veidz <joaovictorveidz@gmail.com>
2022-09-17 17:33:44 -0300

    refactor: create a function to isolate the array print

--- selection-sort.c
+++ selection-sort.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
 void insertionSort(int array[], int length);
+void printArray(int array[], int length);
 
 int main() {
   int array[] = { 5, 3, 1, 8, 0 };
   int length = sizeof(array) / sizeof(array[0]);
 
+  printf("Original Array\n");
+  printArray(array, length);
+
   insertionSort(array, length);
 
+  printf("\n\nSorted Array\n");
+  printArray(array, length);
+}
+
+void printArray(int array[], int length) {
   for (int i = 0; i < length; i++) {
     printf("%d ", array[i]);
   }
