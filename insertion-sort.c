fb19c7297ff46ae5e061664fe0fc65c91ee62ab5
Veidz <joaovictorveidz@gmail.com>
2022-09-17 15:41:25 -0300

    feat: add insertion sort implementation

---
+++ insertion-sort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  int array[] = { 5, 3, 1, 8, 0 };
+  int length = sizeof(array) / sizeof(array[0]);
+
+  int i, j, aux;
+
+  for (i = 1; i < length; i++) {
+    aux = array[i];
+
+    for (j = i; aux < array[j - 1]; j--) {
+      array[j] = array[j - 1];
+    }
+
+    array[j] = aux;
+  }
+
+  for (i = 0; i < length; i++) {
+    printf("%d ", array[i]);
+  }
+}
