2a877b4498a0c7ca11acdc176701d18f99d5d6ad
Veidz <joaovictorveidz@gmail.com>
2022-09-17 23:39:18 -0300

    feat: add dynamic array size implementation

--- quick-sort.c
+++ quick-sort.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+void initializeArray(int *array, int length);
 void swap(int *x, int *y);
 void quickSort(int array[], int length);
 void quickSortRecursion(int array[], int low, int high);
 int partition(int array[], int low, int high);
+void printArray(int array[], int length);
 
 int main() {
-  int array[] = { 5, 3, 1, 8, 0 };
-  int length = sizeof(array) / sizeof(array[0]);
+  int *array;
+  int length = 10;
 
-  quickSort(array, length);
+  array = (int*)malloc(sizeof(int) * length);
 
+  if (array != NULL) {
+    initializeArray(array, length);
+
+    printf("Original Array\n");
+    printArray(array, length);
+
+    quickSort(array, length);
+
+    printf("\nSorted Array\n");
+    printArray(array, length);
+  }
+  else {
+    printf("Memory not allocated\n");
+    exit(0);
+  }
+}
+
+void initializeArray(int *array, int length) {
+  srand(time(NULL));
+  for (int i = 0; i < length; i++) {
+    array[i] = rand() % 10;
+  }
+}
+
+void printArray(int array[], int length) {
   for (int i = 0; i < length; i++) {
     printf("%d ", array[i]);
   }
